117bfea2537dafc2d855d3f7bc6c732a3898c1ce
Enpas <enniopas2002@libero.it>
2021-05-21 18:07:10 +0200

    main con card fatto - completo

---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+
+    <div class="cards">
+
+      <div class="box-imgs">
+        <img :src="singleCard.thumb" :alt="singleCard.series">
+      </div>
+      <h3>{{singleCard.series}}</h3>
+      
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    singleCard: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/vars.scss';
+
+.cards {
+  flex-basis: calc(100% / 6);
+  margin-right: 20px;
+  margin-bottom: 40px;
+  .box-imgs {
+    height: 190px;
+    overflow-y: hidden;
+    margin-bottom: 20px;
+  }
+  h3 {
+    max-width: 190px;
+    font-size: 14px;
+    font-weight: 400;
+    text-transform: uppercase;
+    color: #FFF;
+  }
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,27 +2,63 @@
   <main>
 
     <div class="container">
-      <h2>Content goes here</h2>
+     <a href="#" class="current-series">Current series</a>
+      <div class="box-card">
+        <Card 
+          v-for="(comic, index) in comics" :key="index"
+          :singleCard="comic"
+        />
+      </div>
+      <a href="#" class="load-more">load more</a>
     </div>
 
   </main>
 </template>
 
 <script>
+import Card from '@/components/Card.vue'
+import comics from '@/assets/data/dc-comics.js'
+
 export default {
   name: 'Main',
+   data() {
+    return {
+      comics
+    }
+  },
+  components: {
+    Card
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   @import '../assets/style/vars.scss';
+  @import '../assets/style/mixins.scss';
 
   .container {
     background-color: $dark;
     padding-top:62px;
-    padding-bottom:62px;
-    h2 {
-      color: #FFF;
+    padding-bottom: 30px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .box-card {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+    }
+    .current-series {
+      @include buttonMain();
+      font-size: 22px;
+      position: absolute;
+      left: 300px;
+      top: -28px
+    }
+    .load-more {
+      @include loadBtn();
+      font-size: 14px;
     }
   }
   
